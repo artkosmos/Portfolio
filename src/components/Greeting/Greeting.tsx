132ab71cbef95804063618b3f135fec65fbaf2a4
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-07-06 19:04:43 +0300

    refactor greeting structure

--- src/components/Greeting/Greeting.tsx
+++ src/components/Greeting/Greeting.tsx
@@ -4,14 +4,14 @@ import styleContainer from '../../container/container.module.scss'
 export const Greeting = () => {
   return (
     <article className={style.greeting}>
-      <div className={`${style.greetingContent} ${styleContainer.container}`}>
+      <div className={`${styleContainer.container} ${style.greetingContent}`}>
         <div className={style.text}>
-          <span>Hi there!</span>
-          <h1>I am Artem Kosmatkov</h1>
-          <span>Frontend Developer</span>
+          <span className={style.hello}>Hi there!</span>
+          <h1 className={style.name}>I am Artem Kosmatkov</h1>
+          <span className={style.profession}>Frontend Developer</span>
         </div>
         <div className={style.image}>
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg/274px-Elon_Musk_Royal_Society_%28crop2%29.jpg" alt="my photo"/>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg/274px-Elon_Musk_Royal_Society_%28crop2%29.jpg" alt="my_photo"/>
         </div>
       </div>
     </article>
