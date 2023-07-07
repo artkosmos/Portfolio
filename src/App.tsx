ed73af0f1a608e004e3fe92c42a4e25427655fad
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-07-07 21:00:31 +0300

    implement project section structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.module.scss';
 import {Header} from "./components/Header/Header";
 import {Greeting} from "./components/Greeting/Greeting";
 import {Skills} from "./components/Skills/Skills";
+import {Projects} from "./components/Projects/Projects";
 
 function App() {
   return (
@@ -9,8 +10,9 @@ function App() {
       <Header/>
       <Greeting/>
       <Skills/>
+      <Projects/>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Projects/Project/Project.tsx
@@ -0,0 +1,21 @@
+import style from './Project.module.scss'
+
+type ProjectPropsType = {
+  title: string
+}
+
+export const Project = ({title}: ProjectPropsType) => {
+  return (
+    <div className={style.project}>
+      <div className={style.image}>
+        <button className={style.button}>Look at the project</button>
+      </div>
+      <h3 className={style.projectTitle}>{title}</h3>
+      <div className={style.description}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Excepturi ipsum nemo nihil officiis repudiandae. Ad culpa laudantium nobis quasi sequi.
+        Adipisci, aliquid amet commodi ipsam iste maiores optio recusandae veniam.
+      </div>
+    </div>
+  )
+}
