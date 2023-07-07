0cc81e96604488fc0904272af3c1743201f91933
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-07-07 21:01:21 +0300

    style refactor

--- src/components/Greeting/Greeting.tsx
+++ src/components/Greeting/Greeting.tsx
@@ -11,9 +11,7 @@ export const Greeting = () => {
             <h1 className={style.name}>I am Artem Kosmatkov</h1>
             <span className={style.profession}>Frontend Developer</span>
           </div>
-          <div className={style.image}>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg/274px-Elon_Musk_Royal_Society_%28crop2%29.jpg" alt="my_photo"/>
-          </div>
+          <div className={style.image}></div>
         </div>
       </div>
     </section>
