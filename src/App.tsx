20e06e81b59b09cfa61d149595fd358240339c22
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-07-08 14:15:30 +0300

    implement contacts section structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {Header} from "./components/Header/Header";
 import {Greeting} from "./components/Greeting/Greeting";
 import {Skills} from "./components/Skills/Skills";
 import {Projects} from "./components/Projects/Projects";
+import {Contacts} from "./components/Contacts/Contacts";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Greeting/>
       <Skills/>
       <Projects/>
+      <Contacts/>
     </div>
   )
 }
